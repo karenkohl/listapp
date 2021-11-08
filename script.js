792ee161472ad1e154bb9d05fc4e0aa5a5a877fa
karenkohl <jackson.karenc@gmail.com>
2021-11-08 11:38:07 -0800

    roughed in local storege changed some styling

--- script.js
+++ script.js
@@ -13,25 +13,32 @@ function add(newItem) {
         i.addEventListener("click", function(e) {
             listItem.remove(e.target)
             listItem.remove(e.parent)
+            todoObjectList.splice(todoObjectList.findIndex(todos => todos.text === newItem), 1)
+            localStorage.setItem('todos', JSON.stringify(todoObjectList))
+            
         })
         listItem.textContent = newItem
         listItem.appendChild(i)
-        todoObjectList.shift(newItem)
+        //todoObjectList.shift(newItem)
         ul.appendChild(listItem) 
         listItem.className = "todoItem"
         input.value = ""
+    
 
 
         let ol = document.createElement("ol")
         ol.className = "subList"
         listItem.appendChild(ol)
     
-        createSublistItem(ol)
+        const todoItem = { text: newItem, subtasks: [] }
+        todoObjectList.push(todoItem)
+        localStorage.setItem('todos', JSON.stringify(todoObjectList))
+        createSublistItem(ol, todoItem)
         
     }
 }
 
-const createSublistItem = (parent) => {
+const createSublistItem = (parent, todoItem) => {
     let inputField = document.createElement("input")
     let newButton = document.createElement("span")
     parent.appendChild(newButton)
@@ -41,6 +48,8 @@ const createSublistItem = (parent) => {
     newButton.addEventListener("click", function(e) {
         e.stopPropagation()
         let subListItem = document.createElement("li")
+        todoItem.subtasks.push(inputField.value)
+        localStorage.setItem('todos', todoObjectList)
         subListItem.textContent = inputField.value
         parent.appendChild(subListItem)
         subListItem.addEventListener("click", function() {
@@ -60,3 +69,5 @@ const createSublistItem = (parent) => {
 const addToList = document.querySelector("#add_button").addEventListener("click", function() {
     add(input.value)
 })
+
+console.log(todoObjectList)
